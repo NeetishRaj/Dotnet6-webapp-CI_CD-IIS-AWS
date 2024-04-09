3f4c00cb48897d312d1ad4336a0a242408413aa9
Neetish Raj <neetishop@gmail.com>
2024-04-09 05:32:35 +0530

    log update

--- scripts/start_other_services.ps1
+++ scripts/start_other_services.ps1
@@ -10,4 +10,4 @@
 $filePath = "C:\test\log.txt"
 $content = "This is a log from CodeDeploy script named 'start_other_services.ps1'"
 
-Set-Content -Path $filePath -Value $content
+Set-Content -Path $filePath -Value $content -force
